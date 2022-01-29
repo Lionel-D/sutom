fb726f61c3d70d231030ca9e86a2aab1328aabf5
JonathanMM <jonathanmm@free.fr>
2022-01-29 15:02:15 +0100

    Ajout d'un générateur d'item

--- ts/configurationPanel.ts
+++ ts/configurationPanel.ts
@@ -20,10 +20,38 @@ export default class ConfigurationPanel {
 
   public afficher(): void {
     let titre = "Configuration";
-    let contenu = "";
+    let contenu = document.createElement("div");
+    contenu.appendChild(
+      this.genererConfigItem("Volume du son (si activé)", {
+        1: "Faible",
+        2: "Normal",
+        3: "Fort",
+      })
+    );
 
-    this._panelManager.setContenu(titre, contenu);
+    this._panelManager.setContenu(titre, contenu.innerHTML);
     this._panelManager.setClasses(["config-panel"]);
     this._panelManager.afficherPanel();
+  }
+
+  private genererConfigItem(nomConfig: string, options: { [value: number]: string }): HTMLElement {
+    let div = document.createElement("div");
+    div.className = "config-item";
+
+    let label = document.createElement("label");
+    label.innerText = nomConfig;
+    div.appendChild(label);
+
+    let select = document.createElement("select");
+    for (let optionKey in options) {
+      let optionLabel = options[optionKey];
+      let optionElement = document.createElement("option");
+      optionElement.value = optionKey;
+      optionElement.innerText = optionLabel;
+      select.appendChild(optionElement);
+    }
+    div.appendChild(select);
+
+    return div;
   }
 }
